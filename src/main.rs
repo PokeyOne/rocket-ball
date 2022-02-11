32688d2762987b23d56f97522b650a3610ac6c6d
Mateo Carreras <mateo.carreras@gmail.com>
2022-02-10 22:32:00 -0800

    Added a person and name component and a system to add them based on the tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 use bevy::prelude::*;
 
-fn say_hello() {
-    println!("Hello, World!");
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    let names: Vec<String> = vec!["Bob", "Rob", "Dob"].into_iter().map(|s| s.to_string()).collect();
+
+    for name in names {
+        commands.spawn().insert(Person).insert(Name(name));
+    }
+}
+
+fn say_hello(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}", name.0);
+    }
 }
 
 fn main() {
     App::new()
+        .add_startup_system(add_people)
         .add_system(say_hello)
         .run();
 }
