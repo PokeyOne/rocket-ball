3d872faade4868b09c2f9ead1ab65b3291fed999
Mateo Carreras <mateo.carreras@gmail.com>
2022-02-11 00:36:52 -0800

    added box for ball

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    let names: Vec<String> = vec!["Bob", "Rob", "Dob"].into_iter().map(|s| s.to_string()).collect();
-
-    for name in names {
-        commands.spawn().insert(Person).insert(Name(name));
-    }
+struct Position {
+    x: f64,
+    y: f64
 }
 
-fn say_hello(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}", name.0);
-    }
+#[derive(Component)]
+struct Velocity {
+    x: f64,
+    y: f64
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // cameras
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(UiCameraBundle::default());
+
+    // Ball
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: Vec3::new(30.0, 30.0, 0.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::rgb(1.0, 1.0, 1.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        });
 }
 
 fn main() {
     App::new()
-        .add_startup_system(add_people)
-        .add_system(say_hello)
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .run();
 }
